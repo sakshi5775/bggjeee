ddb936b3510243fbad309f1065e8d7044c3c7fd8
mamoon-acm <mamoon.acm@gmail.com>
2026-03-10 18:45:03 +0530

    work on catching on get api

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:hive_ce/hive.dart';
 import 'package:upgrader/upgrader.dart';
 // import 'package:astrobharataiuser/core/services/notification_service.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
@@ -38,6 +39,7 @@ import 'package:astrobharataiuser/core/services/crashlytics_service.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:astrobharataiuser/core/controllers/global_nav_controller.dart';
 import 'package:astrobharataiuser/screens/user_dashboard/widgets/user_bottom_nav.dart';
+import 'package:hive_ce_flutter/hive_ce_flutter.dart';
 
 List<Locale>? _cachedSupportedLocales;
 
@@ -47,6 +49,8 @@ void main() {
       // 1. Initialize bindings inside the same zone as runApp
       WidgetsFlutterBinding.ensureInitialized();
 
+      await Hive.initFlutter();
+      await Hive.openBox('api_cache'); // Global cache box
       // 2. Prevent zone-related bugs by ensuring consistency
       BindingBase.debugZoneErrorsAreFatal = true;
 
